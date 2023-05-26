f0d17403659c5bec42e692f80c46ea56e2e6c5fd
oustr <biu_biu_@outlook.com>
2023-05-26 23:36:08 +1000

    update

--- _next/static/QefoFHfbifN1lfxh9bGfB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1f0db4e69d5ac7a7.css","static/chunks/pages/index-ada482bb293fbcc6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
